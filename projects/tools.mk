5dcc62b408214f5b8bb96cce4b0b521aca707417
wangyc@risetek.com <wangyc@risetek.com>
2021-08-18 17:20:38 +0800

    debug on kernel module

--- projects/tools.mk
+++ projects/tools.mk
@@ -1,8 +1,12 @@
 export TOP_TOOLCHAIN=/home/wangyc/github/risetek.r64tools/toolchain
+export TOPDIR=/home/wangyc/github/risetek.r64tools/toolchain
+export INCLUDE_DIR=$(TOP_TOOLCHAIN)/include
 export STAGING_DIR=$(TOP_TOOLCHAIN)/staging_dir
 export TOOLCHAIN_DIR=$(STAGING_DIR)/toolchain-aarch64_cortex-a53_gcc-8.4.0_musl
 export LDCFLAGS=$(TOOLCHAIN_DIR)/usr/lib
 export LD_LIBRARY_PATH=$(TOOLCHAIN_DIR)/usr/lib
+export PKG_BUILD_DIR=/home/wangyc/github/risetek.r64tools/projects/tmp
+export LINUX_KMOD_SUFFIX=ko
 
 AARCH64_CROSS_COMPILE?=aarch64-openwrt-linux-
 CROSS_COMPILE=$(TOOLCHAIN_DIR)/bin/$(AARCH64_CROSS_COMPILE)
